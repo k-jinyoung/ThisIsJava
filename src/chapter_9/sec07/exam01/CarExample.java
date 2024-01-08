8125cfad89bc4f8a2a7f85ce56a8c832fd93dc08
김진영 <154316245+k-jinyoung@users.noreply.github.com>
2024-01-08 17:48:50 +0900

    chapter_09

---
+++ src/chapter_9/sec07/exam01/CarExample.java
@@ -0,0 +1,22 @@
+package chapter_9.sec07.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		car.run1();
+		
+		car.run2();
+		
+		//Tire tire = new Tire();
+		car.run3(new Tire() {
+			@Override
+			public void roll() {
+				System.out.println("익명 자식 Tire 객체 3이 굴러갑니다.");
+			}
+		});
+
+	}
+
+}
