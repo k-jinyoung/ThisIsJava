6331c9e61b1f4dc1aaa09e19807dbe9f07342589
김진영 <154316245+k-jinyoung@users.noreply.github.com>
2024-01-16 16:29:51 +0900

    chapter_15_15.5

---
+++ src/chapter_15/sec05/exam04/Fruit.java
@@ -0,0 +1,14 @@
+package chapter_15.sec05.exam04;
+
+public class Fruit {
+	//필드 선언
+	public String name;
+	public int price;
+	
+	//생성자
+	public Fruit(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+}
---
+++ src/chapter_15/sec05/test01/testExample.java
@@ -0,0 +1,17 @@
+package chapter_15.sec05.test01;
+
+public class testExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer x = 3;
+		Integer y = 4;
+		
+		System.out.println(x.compareTo(y));
+		System.out.println(x.compareTo(3));
+		System.out.println(x.compareTo(2));
+
+	}
+
+}
